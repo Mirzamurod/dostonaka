33280c588b908e8c377613888c9e62acb09f400c
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2024-01-02 13:22:01 +0500

    changed name

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export const metadata: Metadata = {
-  title: 'Teeth',
+  title: 'Kirma',
   description: 'Best Teeth App in the world',
   icons: { apple: './images/teeth.png', icon: './images/teeth.png' },
   manifest: './manifest.json',
